f7c2cf7214159874870d9a6789fef8961d4c4b1a
nana <a.a.zazulina@gmail.com>
2024-05-13 23:19:10 +0300

    intermediate

--- routes/dashboardRoutes.js
+++ routes/dashboardRoutes.js
@@ -12,17 +12,19 @@ router.get('/dashboard', async (req, res) => {
         return res.status(500).send("Internal Server Error: User ID is undefined");
     }
     try {
-        const page = parseInt(req.query.page) || 1; // Get the page number from query or default to 1
-        const limit = 20; // You can also make this dynamic if needed
-        const { rows, totalPages } = await Book.findAll({ page, limit });
+        const page = parseInt(req.query.page) || 1;
+        const limit = 20;
+        const { rows, totalItems, totalPages } = await Book.findAll({ page, limit: 20 });
         const roleIds = await User.getUserRoles(req.session.user.userId);
         const roleNames = await User.getRoleNames(roleIds);
 
         res.render('dashboard', {
             user: req.session.user,
             books: rows,
+            totalItems: totalItems,
             totalPages: totalPages,
-            roleNames: roleNames
+            roleNames: roleNames,
+            currentPage: page
         });
     } catch (error) {
         console.error('Error loading books:', error);
